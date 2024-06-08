c1aacf67c09b593a7a29c5be11c653f560144e40
Filtux <96967851+Filtux@users.noreply.github.com>
2024-06-08 10:38:58 +0930

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            minify: false,
+        }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+};
